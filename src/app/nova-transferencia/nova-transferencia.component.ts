8f0041d6de4ae973200018c33a9aac4d7a3a2410
ThiSBernardo <93749791+ThiSBernardo@users.noreply.github.com>
2022-06-09 13:28:20 -0300

    definição dos valores

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -8,13 +8,22 @@ import { Component } from "@angular/core";
 
 export class NovaTransferenciaComponent {
 
-valor: number = 12;
-destino: number = 222;
+  @Output() aoTransferir = new EventEmitter<any>();
 
-transferir(){
-  console.log('transferencia solicitada com sucesso');
-  console.log('Valor: ', this.valor);
-  console.log('Destino: ', this.destino);
+  valor: number;
+  destino: number;
 
-}
+  transferir() {
+    console.log('transferencia solicitada com sucesso');
+    
+    const valorEmitir = { valor: this.valor, destino: this.destino };
+    this.aoTransferir.emit(valorEmitir);
+    
+    this.limparCampos();
+  }
+
+  limparCampos() {
+    this.valor = 0;
+    this.destino = 0;
+  }
 }
